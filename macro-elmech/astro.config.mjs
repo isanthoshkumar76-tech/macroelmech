ff542670b0ef1280608a194840fdea9a6db2bcc1
isanthosh-dev <isanthoshkumar76@gmail.com>
2026-02-12 00:08:59 +0530

    Restore project files

--- macro-elmech/astro.config.mjs
+++ macro-elmech/astro.config.mjs
@@ -1,13 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://isanthoshkumar76-tech.github.io',
-base: '/macroelmech/',
+  base: '/macroelmech/',
+  output: 'static',
   devToolbar: { enabled: false },
   vite: { plugins: [tailwindcss()] },
   integrations: [react()],
-    output: 'static'
 });
